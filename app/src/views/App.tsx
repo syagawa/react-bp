00143a7541aea310bdfa969833be2e0e10f220c3
yagawa <yagawa@qzc.co.jp>
2020-01-07 19:00:51 +0900

    Fix App.test.tsx

--- app/src/views/App.tsx
+++ app/src/views/App.tsx
@@ -1,13 +1,6 @@
 import React, { FC } from "react";
-import {
-  Route,
-  Switch,
-  Redirect
-} from "react-router-dom";
-import {
-  MuiThemeProvider,
-  createMuiTheme
-} from "@material-ui/core/styles";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import red from "@material-ui/core/colors/red";
 
 import Todos from "./containers/Todos";
